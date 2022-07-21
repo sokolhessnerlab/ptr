49de107d673ed809baaae2c17b2a897968d9d2cc
miacudahy <82066673+miacudahy@users.noreply.github.com>
2022-07-21 15:58:16 -0500

    Loading Data and Creating Columns and Variables

---
+++ R/PTRanalysis.R
@@ -0,0 +1,50 @@
+# Your functions can go here. Then, when you want to call those functions, run
+#### SETUP ####
+rm(list=ls()) # CLEAR THE WORKSPACE
+setwd('/Users/mia.cudahy1/Documents/MATLAB/')
+
+#### Load the Data and Add Columns#### locally 
+
+PTRPart1_data <- as.matrix(read.table("PTRPart1_data_738719.7960.txt", sep = ",", header = TRUE, 
+                                      col.names = c("subjID", "trial", "offer", "RT", "totalrec", 
+                                                    "partnerid", "partnerresp", "PA", "PRec", "PG", "PR")));
+PTRPart1_data <- data.frame(PTRPart1_data);
+
+PTRPart2_data <- as.matrix(read.table("PTRPart2_data_738719.7960.txt", sep = ",", header = TRUE, 
+                                      col.names = c("subjID", "trial", "share/keep", "RT", "totalrec",
+                                                    "partnerid", "partneroff", "PA", "PRec", "PG", "PR")));
+
+PTRPart2_data <- data.frame(PTRPart2_data);
+
+PTRPostQ_data <- as.matrix(read.table("PTRPOSTQPartner_data_738720.7612.txt", sep = ",", header = TRUE, 
+                                      col.names = c("subjID", "partnerid", "avgoffer1", "timepshare1", "pgoodbad1",
+                                                    "avgpoffer2", "timepshare2", "pgoodbad2")));
+PTRPostQ_data <- data.frame(PTRPostQ_data);
+
+PTRRWA_SDO_Demo_Data <- read.csv("PTR_RWA_SDO_Data.csv");
+
+#Look at the data
+head(PTRPart1_data)
+head(PTRPart2_data)
+head(PTRPostQ_data)
+head(PTRRWA_SDO_Demo_Data)
+
+#Creating basic variables 
+# PTRPart1 Data
+number_of_subjects = length(unique(PTRPart1_data$subjID))
+subject_IDs = unique(PTRPart1_data$subjID)
+subject_age = PTRRWA_SDO_Demo_Data$Age
+subject_gender = PTRRWA_SDO_Demo_Data$Gender0M1F
+subject_demographics = data.frame(subject_IDs, subject_age, subject_gender)
+
+# PTRPart1 Data
+
+#PTRPart2 Data
+
+#PTRPostQ Data
+
+
+
+
+# `source('R/functions.R')` within a code block in the RMarkdown notebook.
+print("Your scripts and functions should be in the R folder.")
--- R/functions.R
+++
@@ -1,3 +0,0 @@
-# Your functions can go here. Then, when you want to call those functions, run
-# `source('R/functions.R')` within a code block in the RMarkdown notebook.
-print("Your scripts and functions should be in the R folder.")
